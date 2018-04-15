b4620c65197eb851cf7c0a3d4a843710e0ba72ff
Hubert Grzesiak <hubert.grzesiak@outlook.com>
2018-04-15 20:08:27 +0200

    login views
    Add login views, pages

--- wateringApp/urls.py
+++ wateringApp/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'wateringApp'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('singIn', views.singIn, name='singIn'),
+    path('singOut', views.singOut, name='singOut'),
+    path('readOut', views.readOut, name='readOut')
 ]
--- wateringApp/views.py
+++ wateringApp/views.py
@@ -1,5 +1,39 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login
 
+from django.contrib.auth import login, logout
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib import messages
+from wateringApp.models import Statistics
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    """Strona główna aplikacji."""
+    # return HttpResponse("Witaj w aplikacji!")
+    return render(request, 'wateringApp/index.html')
+
+def singIn(request):
+    """Logowanie"""
+    from django.contrib.auth.forms import authentication
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            messages.success(request,"Zostałeś zalogowany!")
+            return redirect(reverse('wateringApp:index'))
+        
+    context = {'form': AuthenticationForm()}
+    return render(request,'wateringApp/singIn.html', context)
+
+def singOut(request):
+    """Wylogowywanie"""
+    logout(request)
+    messages.info(request,"Zostałeś wylogowany!")
+    return redirect(reverse('wateringApp:index'))
+
+def readOut(request):
+    """Odczyty"""
+    sensors = Statistics.objects.all()
+    context = {'sensors': sensors}
+    return render(request, 'wateringApp/readOut.html', context)
